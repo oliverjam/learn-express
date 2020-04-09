87c03cedfbefce22109e00a0b7f0bbad2076efd6
oliverjam <oliverjamesphillips@gmail.com>
2020-04-09 17:16:21 +0100

    Fix relative CSS link

--- solution/templates.js
+++ solution/templates.js
@@ -6,7 +6,7 @@ function layout(content) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Learn Express</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="/style.css">
       </head>
       <body>
         <header>
